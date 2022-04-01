fe5ce133e1fd5cb6dfa043616c2a7668ffc3f92c
pengheyan <pengheyan@cennavi.com.cn>
2022-04-01 17:20:41 +0800

    feature(all): 修改vite.config.js，添加了一些配置

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
+<!--
+ * @Description: 
+ * @Author: 
+ * @Date: 2022-03-31 17:42:43
+ * @LastEditTime: 2022-04-01 15:07:12
+ * @LastEditors: PengHeyan
+ * @Usage: 
+-->
 # Vue 3 + Vite
-
-This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
-
-## Recommended IDE Setup
-
-- [VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=johnsoncodehk.volar)
+## 🛠 集成
+* vite
+* vue3.0
+* axios
+* echarts
+* lodash
+* moments
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
  * @Description:
  * @Author:
  * @Date: 2022-03-31 16:38:17
- * @LastEditTime: 2022-03-31 17:00:23
+ * @LastEditTime: 2022-04-01 17:18:31
  * @LastEditors: PengHeyan
  * @Usage:
  */
@@ -10,12 +10,21 @@ import { resolve } from "path"; // 主要用于alias文件路径别名
 import { defineConfig, loadEnv } from "vite";
 import VueSetupExtend from "vite-plugin-vue-setup-extend";
 import vue from "@vitejs/plugin-vue";
+import ViteImages from "vite-plugin-vue-images";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, __dirname);
   return {
-    plugins: [vue(), VueSetupExtend()],
+    plugins: [
+      vue({
+        refTransform: true, // 开启ref转换
+      }),
+      VueSetupExtend(),
+      ViteImages({
+        dirs: ["src/assets/img/map"], // 指明图片存放目录
+      }),
+    ],
     //打包资源基本路径
     base: env.VITE_BASE_URL,
     css: {
